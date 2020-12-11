a7dd520bb936eedfb65e077614e6d6fe77e17cdc
oleg-kazbeev <kazbeev@gmail.com>
2020-12-11 12:50:36 +0700

    Delete unuseful commented code.

--- terminal_parser.py
+++ terminal_parser.py
@@ -12,26 +12,3 @@ class TerminalParser(argparse.ArgumentParser):
     def get_list_of_output_files(self, terminal_command):
         _options = self._get_options_from_command_line(terminal_command)
         return _options.output
-
-
-# def create_parser():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('-i', '--input', default=[], nargs='+')
-#     parser.add_argument('-o', '--output', default=[], nargs='+')
-#
-#     return parser
-#
-#
-# def parse_terminal_command():
-#     parser = create_parser()
-#     options = parser.parse_args(sys.argv[1:])
-#
-#     return options
-#
-#
-# def get_list_of_input_files(options):
-#     return options.input
-#
-#
-# def get_list_of_output_files(options):
-#     return options.output
